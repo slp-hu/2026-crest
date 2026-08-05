80241509dbcca8063c5aaa79fd48e16a4207634a
slp-hu <itou@hosei.ac.jp>
2026-08-06 00:18:19 +0900

    Add files via upload

---
+++ demo2608/audio/audio_index.js
@@ -0,0 +1,38 @@
+// build_explainer_audio.py が生成。explainer_p2.html が <script src> で読む。
+window.EXPLAINER_AUDIO = {
+ "spk": "1004",
+ "uid": "f64c4c5984",
+ "dir": "./audio/",
+ "items": {
+  "A1": {
+   "file": "A1.wav",
+   "dur": 12.19
+  },
+  "D_mos": {
+   "file": "D_mos.wav",
+   "dur": 10.809
+  },
+  "D_real": {
+   "file": "D_real.wav",
+   "dur": 11.413
+  },
+  "D_avg10": {
+   "file": "D_avg10.wav",
+   "dur": 11.9
+  },
+  "D_mosg15": {
+   "file": "D_mosg15.wav",
+   "dur": 9.961
+  },
+  "D_mosK2": {
+   "file": "D_mosK2.wav",
+   "dur": 12.005
+  },
+  "R": {
+   "file": "R.wav",
+   "dur": 11.308,
+   "target": "1249"
+  }
+ },
+ "text": "とまず無人島っていうのが日頃本当に馴染みがなくてどういう島を想定するか凄い考えたんですけど私は高校の時にワイエムシーエーのキャンプに行ったことがありまして"
+};
