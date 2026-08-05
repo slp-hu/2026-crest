4d0e226ea9382c7eddd172e329d79de28ebacb96
slp-hu <itou@hosei.ac.jp>
2026-08-06 01:26:11 +0900

    Add files via upload

--- demo2608/audio/audio_index.js
+++ demo2608/audio/audio_index.js
@@ -1,45 +1,61 @@
 // build_explainer_audio.py が生成。explainer_p2.html が <script src> で読む。
 window.EXPLAINER_AUDIO = {
- "spk": "1004",
- "uid": "f64c4c5984",
+ "spk": "1011",
+ "uid": "e830cf8218",
  "dir": "./audio/",
  "items": {
+  "src": {
+   "file": "src.wav",
+   "dur": 6.405
+  },
   "A1": {
    "file": "A1.wav",
-   "dur": 12.19
+   "dur": 6.594
   },
   "D_mos": {
    "file": "D_mos.wav",
-   "dur": 10.809
+   "dur": 7.117
   },
   "D_real": {
    "file": "D_real.wav",
-   "dur": 11.413
+   "dur": 7.129
   },
   "D_avg10": {
    "file": "D_avg10.wav",
-   "dur": 11.9
+   "dur": 7.454
   },
   "D_mosg15": {
    "file": "D_mosg15.wav",
-   "dur": 9.961
+   "dur": 6.966
   },
   "D_mosK2": {
    "file": "D_mosK2.wav",
-   "dur": 12.005
+   "dur": 7.303
+  },
+  "VC_self": {
+   "file": "VC_self.wav",
+   "dur": 6.4
+  },
+  "VC_mos": {
+   "file": "VC_mos.wav",
+   "dur": 6.4
+  },
+  "VC_mosK2": {
+   "file": "VC_mosK2.wav",
+   "dur": 6.4
   },
   "R": {
    "file": "R.wav",
-   "dur": 12.19,
-   "target": "1004",
+   "dur": 6.594,
+   "target": "1011",
    "correct": true
   },
   "R_wrong": {
    "file": "R_wrong.wav",
-   "dur": 11.401,
-   "target": "1231",
+   "dur": 7.129,
+   "target": "1257",
    "correct": false
   }
  },
- "text": "とまず無人島っていうのが日頃本当に馴染みがなくてどういう島を想定するか凄い考えたんですけど私は高校の時にワイエムシーエーのキャンプに行ったことがありまして"
+ "text": "はいえーあのー千九百九十五年二月十七日私のお世話になってる方の出版記念パーティーがありました"
 };
